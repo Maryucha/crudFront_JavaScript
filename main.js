7597b11dfde3d6cbfba772dcf3ef3533030d9e36
Maryucha_Mariani <https://Maryucha_Mariani@bitbucket.org/Maryucha_Mariani/appsensores.git>
2021-08-12 20:08:45 -0300

    subindo as imagens

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
 'use strict';
 
 const openModal = () => document.getElementById('modal').classList.add('active');
-
 const closeModal = () => document.getElementById('modal').classList.remove('active');
 
+const tempClient = {
+    nome: 'maryucha',
+    email: 'mary@gmail.com',
+    celular: '48 98427-6739',
+    cidade: "São josé"
+}
+
+const criarCliente = (cliente) => {
+    localStorage.setItem('teste', 'teste crud');
+}
+ 
 
 document.getElementById('cadastrarCliente').addEventListener('click', openModal);
-
 document.getElementById('modalClose').addEventListener('click', closeModal);
