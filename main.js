f1c591c2b2ed1df49ddc01a670492890395bc05e
Maryucha_Mariani <https://Maryucha_Mariani@bitbucket.org/Maryucha_Mariani/appsensores.git>
2021-08-13 15:06:17 -0300

    subindo para o local storage

--- main.js
+++ main.js
@@ -4,16 +4,20 @@ const openModal = () => document.getElementById('modal').classList.add('active')
 const closeModal = () => document.getElementById('modal').classList.remove('active');
 
 const tempClient = {
-    nome: 'maryucha',
-    email: 'mary@gmail.com',
+    nome: 'Lipe',
+    email: 'lip@gmail.com',
     celular: '48 98427-6739',
     cidade: "São josé"
 }
 
-const criarCliente = (cliente) => {
-    localStorage.setItem('teste', 'teste crud');
+const getLocalStorage = () => JSON.parse(localStorage.getItem('db_local')) ?? []
+const setLocalStorage = (dbClient) => localStorage.setItem("db_local", JSON.stringify(dbClient));
+
+const criarCliente = (client) => {
+    const bdCliente = getLocalStorage();
+    bdCliente.push(client);
+    setLocalStorage(bdCliente);
 }
- 
 
 document.getElementById('cadastrarCliente').addEventListener('click', openModal);
 document.getElementById('modalClose').addEventListener('click', closeModal);
