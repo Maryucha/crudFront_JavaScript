551f30cef812ae5f4964fe6379137dc6ce64ded6
Maryucha_Mariani <https://Maryucha_Mariani@bitbucket.org/Maryucha_Mariani/appsensores.git>
2021-08-14 10:51:40 -0300

    validando campos do formulário de novo cliente

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ const openModal = () => document.getElementById('modal').classList.add('active')
 const closeModal = () => document.getElementById('modal').classList.remove('active');
 
 const tempClient = {
-    nome: 'Lipe',
-    email: 'lip@gmail.com',
+    nome: 'Gab',
+    email: 'gab@gmail.com',
     celular: '48 98427-6739',
     cidade: "São josé"
 }
@@ -13,13 +13,43 @@ const tempClient = {
 const getLocalStorage = () => JSON.parse(localStorage.getItem('db_local')) ?? []
 const setLocalStorage = (dbClient) => localStorage.setItem("db_local", JSON.stringify(dbClient));
 
-const criarCliente = (client) => {
+const criarClient = (client) => {
     const bdCliente = getLocalStorage();
     bdCliente.push(client);
     setLocalStorage(bdCliente);
 }
 
-const lerCliente = () => getLocalStorage();
+const deletarClient = (index) => {
+    const dbClient = lerClient();
+    dbClient.splice(index, 1)
+    setLocalStorage(dbClient)
+}
+
+const lerClient = () => getLocalStorage();
+
+const updadeClient = (index, client) => {
+    const dbClient = lerClient();
+    dbClient[index] = client;
+    setLocalStorage(dbClient);
+}
+
+const salvarClient = (client) => {
+    if(isValidFillds()){
+        console.log('cadastrando cliente');
+    }
+}
+
+const isValidFillds = () =>{
+    return document.getElementById('form').reportValidity();
+}
+
+const limparCampos = () => {
+    const campos = document.querySelectorAll('.modal-fild');
+    campos.forEach(field => field.value = "");
+    document.getElementById('nome').dataset.index = "new";
+}
 
 document.getElementById('cadastrarCliente').addEventListener('click', openModal);
 document.getElementById('modalClose').addEventListener('click', closeModal);
+document.getElementById('salvar').addEventListener('click',salvarClient);
+document.getElementById('cancelar').addEventListener('click',limparCampos);
